a5a5d1cd7b61eb91b983cc60bc19c81c74e8f50b
Mason Schafer <mason.schafer1@gmail.com>
2022-02-13 02:30:57 -0500

    clean up

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://recur-graphql.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
